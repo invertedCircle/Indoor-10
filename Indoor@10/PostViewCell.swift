2fb5365d43e781d9cbeff7730d3ddc2424dc22f6
Faiyaz Chowdhury <faiyaz777@hotmail.com>
2018-07-23 12:03:45 -0400

    Feed inside Tableview

---
+++ Indoor@10/PostViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostViewCell.swift
+//  Indoor@10
+//
+//  Created by Faiyaz Chowdhury on 7/20/18.
+//  Copyright © 2018 Inverted Circle. All rights reserved.
+//
+
+import UIKit
+
+class PostViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
